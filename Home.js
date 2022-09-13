c64bc6e7e83a24c6da186763005580f1ed91722b
rem1nfo <113349956+rem1nfo@users.noreply.github.com>
2022-09-13 20:12:47 +0200

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,5 +1,5 @@
 //Suppression des barres de scroll
-document.body.style.overflow = "hidden";
+//document.body.style.overflow = "hidden";
 
 // ******** Définition des variables importantes ********
 
@@ -12,14 +12,20 @@ const cursor = document.querySelector("#cursor");
 const game = document.querySelector('#game');
 const context = game.getContext('2d'); // Pour les pixels 
 const grid_context = game.getContext('2d'); // Pour la grille
-game.width = 1500; //1200 600
-game.height = 600;
+game.width = 2000; //1200 600
+game.height = 2000;
 
 
 const size_cell = 10; // Taille d'un côté d'une case
 const colors_list = ["#4f0011", "#880029", "#b73300", "#b97a00", "#b99b28", "#b7b280", "#01784c", "#019458", "#5bad3f", "#02554f", "#00737b", "#01958d", "#1a3a77", "#2868aa", "#2868aa", "#3aa7b0", "#352a8d", "#4e43b9", "#6980b8", "#5e1676", "#83368b", "#a57bb8", "#a00c5c", "#b8295e", "#b96f7b", "#4e3422", "#724d1c", "#b98352", "#000", "#3b3c3c", "#636567", "#FFF"]; // Toutes les couleurs disponibles
 let current_color_choice = "#FFF"; // Code de la couleur sélectionnée
 
+
+let position_on_screen_X = 0;
+let position_on_screen_Y = 0;
+
+let scrollX = 0;
+let scrollY = 0;
 
 
 // ******** Création des div pour couleurs palettte ********
@@ -100,8 +106,12 @@ game.addEventListener('mousemove', function(event){
     const cursorLeft = event.clientX - (cursor.offsetWidth/2);
     const cursorTop = event.clientY - (cursor.offsetHeight/2);
 
-    cursor.style.left = Math.floor(cursorLeft/size_cell) * size_cell + "px";
-    cursor.style.top = Math.floor(cursorTop/size_cell) * size_cell + "px";
+    position_on_screen_X = cursorLeft;
+    position_on_screen_Y = cursorTop;
+    //console.log(cursorLeft+","+cursorTop);
+
+    cursor.style.left = Math.floor((cursorLeft+scrollX)/size_cell) * size_cell + "px";
+    cursor.style.top = Math.floor((cursorTop+scrollY)/size_cell) * size_cell + "px";
 });
 
 
@@ -109,18 +119,32 @@ game.addEventListener('mousemove', function(event){
 
 // ******** Gestion du zoom - dézoom *******
 //up = -1 et down = 1
+let zoom = 1; //Zoom initial
 game.addEventListener("wheel", function(event){
     //on cherche si c'est un scroll up ou scroll down
     const delta = Math.sign(event.deltaY);
-    console.log(game.offsetTop);
 
-    if(delta<0){
+
+    // Fonctionne mais plusieurs problèmes :
+    // 1) La taille des pixels n'est plus constante
+    // 2) on ne peut pas se déplacer avec le zoom.
+    /*if(delta<0 && zoom <= 5){
         console.log("zoom");
+        game.style.transform = `scale(${(zoom += 0.05)})`;
     }
-    else{
+    else if(game.getBoundingClientRect().width>=30 && zoom >= 0.5){
         console.log("Dézoom");
-        //context.scale(10, 10);
-    }
+        game.style.transform = `scale(${(zoom -= 0.05)})`;
+    }*/
+});
+
+
+
+// Mise à jour du niveau de scroll actuel -> les coordonnées de la souris sont : x_screen + niveau de scrollX
+window.addEventListener("scroll", (event) => {
+    scrollY = this.scrollY;
+    scrollX = this.scrollX;
+    console.log("scroll en y : "+scrollY+", scroll en x : "+ scrollX);
 });
 
 
@@ -153,7 +177,7 @@ const db = firebase.firestore();
 // ******** Mise à jour des pixels placés par les autres joueurs ********
 db.collection('pixels').onSnapshot(function(querySnapshot){
     querySnapshot.docChanges().forEach(function(change){
-        console.log(change.doc.data());
+        //console.log(change.doc.data());
         const{x,y,color} = change.doc.data();
         context.beginPath();
         context.fillStyle = color;
